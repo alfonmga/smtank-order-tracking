9483b1422215c555381fde2f9f45886f48a0c938
Alfonso Manuel García Astorga <alfonsoimbusiness@gmail.com>
2015-03-31 23:34:08 +0200

    Front-end acabado

--- src/OrderTracking/BackendBundle/Controller/DefaultController.php
+++ src/OrderTracking/BackendBundle/Controller/DefaultController.php
@@ -6,8 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('OrderTrackingBackendBundle:Default:index.html.twig', array('name' => $name));
     }
 }
