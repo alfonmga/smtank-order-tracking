e0b1bc2e177820fa658889e72eaa0ce44a352b34
Alfonso M. García Astorga <hello@alfonsomga.com>
2015-07-26 16:41:59 +0200

    Método para extraer todos los pedidos

--- src/OrderTracking/RestBundle/Controller/DefaultController.php
+++ src/OrderTracking/RestBundle/Controller/DefaultController.php
@@ -4,10 +4,24 @@ namespace OrderTracking\RestBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class DefaultController extends Controller
+use FOS\RestBundle\Controller\FOSRestController,
+    FOS\RestBundle\Controller\Annotations as Rest,
+    FOS\RestBundle\Controller\Annotations\Get,
+    FOS\RestBundle\Controller\Annotations\Post,
+    FOS\RestBundle\Controller\Annotations\Put,
+    FOS\RestBundle\Controller\Annotations\Delete;
+
+class DefaultController extends FOSRestController
 {
-    public function indexAction($name)
+    /**
+     *@Get("/pedidos", name="_api_v1")
+     */
+    public function getPedidosAction()
     {
-        return $this->render('OrderTrackingRestBundle:Default:index.html.twig', array('name' => $name));
+        $pedidos = $this->getDoctrine()->getRepository('OrderTrackingBackendBundle:Pedidos')->findAll();
+
+        return array(
+            'pedidos' => $pedidos,
+        );
     }
 }
