57f0406c723808219e503d38e30d26d123f2852d
Alfonso M. García Astorga <hello@alfonsomga.com>
2015-07-29 00:03:04 +0200

    Eliminar código viejo de la antigua pseudo API.

--- src/OrderTracking/RestBundle/Controller/DefaultController.php
+++ src/OrderTracking/RestBundle/Controller/DefaultController.php
@@ -69,49 +69,6 @@ class DefaultController extends FOSRestController
         return array(
             'form' => $form,
         );
-
-        /*$nombreCliente = $this->get('request')->get('nombre_cliente');
-        $emailCliente = $this->get('request')->get('email_cliente');
-        $nombreProducto = $this->get('request')->get('nombre_producto');
-        $precioProducto = $this->get('request')->get('precio_producto');
-
-        if(!$nombreCliente || !$emailCliente || !$nombreProducto || !$precioProducto) {
-            throw new NotAcceptableHttpException;
-        }
-
-        $em = $this->getDoctrine()->getManager();
-
-        $codigoSeguimiento = '';
-        for ($i = 0; $i < 12; $i++) {
-            $codigoSeguimiento .= rand(0, 1) ? rand(0, 9) : chr(rand(ord('A'), ord('Z')));
-        }
-
-        $pedido = new Pedidos();
-        $pedido->setNombreCliente($nombreCliente);
-        $pedido->setEmailCliente($emailCliente);
-        $pedido->setNombreProducto($nombreProducto);
-        $pedido->setPrecioProducto($precioProducto);
-        $pedido->setEstadoPedido('pendiente');
-        $pedido->setFechaInicio(date_create(date('Y-m-d H:i:s')));
-        $pedido->setCodigoSeguimiento($codigoSeguimiento);
-        $em->persist($pedido);
-        $em->flush();
-
-        $historial = new Historial();
-        $historial->setEstado('pendiente');
-        $historial->setFecha(date_create(date('Y-m-d H:i:s')));
-        $historial->setIdPedido($codigoSeguimiento);
-        $historial->setParentId($pedido);
-
-        $em->persist($historial);
-        $em->flush();
-
-        $this->get('TransactionalEmails')->newPedido($pedido->getNombreCliente(), $pedido->getEmailCliente(), $codigoSeguimiento);
-
-        return array (
-            'estado' => 'success',
-            'codigoSeguimiento' => $pedido->getCodigoSeguimiento()
-        );*/
     }
 
     /**
