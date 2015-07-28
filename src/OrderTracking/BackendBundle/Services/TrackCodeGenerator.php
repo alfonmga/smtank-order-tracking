098824b58821d43e582f442dd89781d582f95c4b
Alfonso M. García Astorga <hello@alfonsomga.com>
2015-07-28 23:56:59 +0200

    Crear generador de códigos de seguimiento como Service

---
+++ src/OrderTracking/BackendBundle/Services/TrackCodeGenerator.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace OrderTracking\BackendBundle\Services;
+
+class TrackCodeGenerator {
+
+    public function generate() {
+        $codigoSeguimiento = '';
+        for ($i = 0; $i < 12; $i++) {
+            $codigoSeguimiento .= rand(0, 1) ? rand(0, 9) : chr(rand(ord('A'), ord('Z')));
+        }
+        return $codigoSeguimiento;
+    }
+}
