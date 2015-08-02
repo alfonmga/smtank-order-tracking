d183e9c80a56439679613ec5933dd4016573369b
Alfonso M. García Astorga <hello@alfonsomga.com>
2015-08-02 01:00:00 +0100

    Renombrar archivo y añadir assertGreaterThan

--- src/OrderTracking/BackendBundle/Tests/Controller/PedidosControllerTest.php
+++ src/OrderTracking/BackendBundle/Tests/Controller/PedidosControllerTest.php
@@ -8,11 +8,16 @@ class PedidosControllerTest extends WebTestCase
 {
     public function testCompleteScenario()
     {
-        // Create a new client to browse the application
+        /**
+         * Comprobar si al intentar acceder al Backend es redireccionado a la página de login
+         */
         $client = static::createClient();
-
-        // Client should be redirected to /login URL
-        $crawler = $client->request('GET', '/backend/');
+        $client->request('GET', '/backend/');
         $this->assertEquals(302, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /backend/");
+        $crawler = $client->followRedirect();
+        $this->assertGreaterThan(
+            0,
+            $crawler->filter('html:contains("Nombre de usuario:")')->count()
+        );
     }
 }
